9b09fcbf13e5023d743e4627dbdabfb810a19a62
yurisi <Dev.yurisi@gmail.com>
2023-10-02 06:07:19 +0900

    あれ

--- src/main/java/space/yurisi/itemhat/ItemHat.java
+++ src/main/java/space/yurisi/itemhat/ItemHat.java
@@ -3,11 +3,13 @@ package space.yurisi.itemhat;
 import org.bukkit.plugin.java.JavaPlugin;
 import space.yurisi.itemhat.command.HatCommand;
 
+import java.util.Objects;
+
 public final class ItemHat extends JavaPlugin{
 
     @Override
     public void onEnable() {
-        getCommand("hat").setExecutor(new HatCommand());
+        Objects.requireNonNull(getCommand("hat")).setExecutor(new HatCommand());
 
         getLogger().info("ItemHatを読み込みました");
     }
